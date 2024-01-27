5e845d88f6d7bd8d2a378546451685cc755ad453
Arturas <arturas.bnr@gmail.com>
2024-01-27 11:49:41 +0200

    added error handling

--- src/Command/ImportLegalEntityTypeCommand.php
+++ src/Command/ImportLegalEntityTypeCommand.php
@@ -6,14 +6,16 @@ namespace App\Command;
 
 use App\Entity\LegalEntityType;
 use Doctrine\ORM\EntityManagerInterface;
+use League\Csv\Exception;
+use League\Csv\InvalidArgument;
+use League\Csv\Reader;
+use League\Csv\UnavailableStream;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
-use Symfony\Component\Serializer\Encoder\CsvEncoder;
-use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
-use Symfony\Component\Serializer\Serializer;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 #[AsCommand(
     name: 'app:import-entity-type',
@@ -21,45 +23,57 @@ use Symfony\Component\Serializer\Serializer;
 )]
 class ImportLegalEntityTypeCommand extends Command
 {
-    public function __construct(protected EntityManagerInterface $entityManager)
-    {
+    public function __construct(
+        private readonly EntityManagerInterface $entityManager,
+        private readonly HttpClientInterface $client,
+    ) {
         parent::__construct();
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $decoder = new Serializer([new ObjectNormalizer], [new CsvEncoder]);
+        try {
+            $path = 'https://www.registrucentras.lt/aduomenys/?byla=JAR_TEI_FORM_KLASIFIKATORIUS.csv';
+            $csv = Reader::createFromString($this->client->request('GET', $path)->getContent());
 
-        $legalEntityTypes = $decoder->decode(file_get_contents(
-            'https://www.registrucentras.lt/aduomenys/?byla=JAR_TEI_FORM_KLASIFIKATORIUS.csv'
-        ),
-            'csv',
-            [CsvEncoder::DELIMITER_KEY => '|']
-        );
+            $csv->setHeaderOffset(0);
+            $csv->setDelimiter('|');
 
-        $legalEntityTypeRepository = $this->entityManager->getRepository(LegalEntityType::class);
-
-        $newEntityTypeCount = 0;
-
-        foreach ($legalEntityTypes as $legalEntityType) {
-            $entityType = $legalEntityTypeRepository->findOneBy(['code' => $legalEntityType['form_kodas']]);
-
-            if (! $entityType) {
-                $this->createLegalEntityType($legalEntityType);
-
-                $newEntityTypeCount++;
+            if (count($csv->getHeader()) === 1) {
+                throw new \Exception("Invalid csv file: $path ");
             }
+
+            $legalEntityTypeRepository = $this->entityManager->getRepository(LegalEntityType::class);
+
+            $legalEntityTypes = $csv->getRecords();
+            $newEntityTypeCount = 0;
+
+            foreach ($legalEntityTypes as $legalEntityType) {
+                $entityType = $legalEntityTypeRepository->findOneBy([
+                    'code' => $legalEntityType['form_kodas'],
+                ]);
+
+                if (! $entityType) {
+                    $this->createLegalEntityType($legalEntityType);
+
+                    $newEntityTypeCount++;
+                }
+            }
+
+            $this->entityManager->flush();
+            $this->entityManager->clear();
+
+            $io = new SymfonyStyle($input, $output);
+
+            $message = $newEntityTypeCount > 0
+                ? 'Newly created records: ' . $newEntityTypeCount . '.'
+                : 'No new entries created.';
+
+            $io->success("Entity types import done. $message");
+        } catch (UnavailableStream|InvalidArgument|Exception $e) {
+            $output->writeln($e->getMessage());
+            return Command::FAILURE;
         }
-
-        $this->entityManager->flush();
-
-        $io = new SymfonyStyle($input, $output);
-
-        $message = $newEntityTypeCount > 0
-            ? 'Newly created records: '.$newEntityTypeCount.'.'
-            : 'No new entries created.';
-
-        $io->success("Entity types import done. $message");
 
         return Command::SUCCESS;
     }
